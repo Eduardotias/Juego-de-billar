3bdbb962b82863368162860a477e755f2cfc02be
Eduardotias <88409677+Eduardotias@users.noreply.github.com>
2022-03-14 20:48:19 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Juego-de-billar
+# AngryBirdsEtapa3
+Angry Birds Etapa 3: Introducción a la Restricción
---
+++ sketch.js
@@ -0,0 +1,64 @@
+var B1limage ,B2limage,B3limage,B4limage,B5limage,B6limage,B7limage;
+var B8image;
+var B9rimage,B10rimage,B11rimage,B12rimage,B13rimage,B14rimage,B15rimage,B16rimage;
+var tableroimage
+
+
+function preload(){
+tablero=loadImage("tablero.PNG");
+B1limage=loadImage("lisa 1.png");
+B2limage=loadImage("lisa 2.png");
+B3limage=loadImage("lisa 3.png");
+B4limage=loadImage("lisa 4.png");
+B5limage=loadImage("lisa 5.png");
+B6limage=loadImage("lisa 6.png");
+B7limage=loadImage("lisa 7.png");
+B8image=loadImage("Bola negra.png");
+B9rimage=loadImage("9 R.png");
+B10rimage=loadImage("10 R.png");
+B11rimage=loadImage("11 R.png");
+B12rimage=loadImage("12 R.png");
+B13rimage=loadImage("13 R.png");
+B14rimage=loadImage("14 R.png");
+B15rimage=loadImage("Rayada 15.png");
+
+
+
+
+}
+
+function setup(){
+createCanvas(350,570);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+}
+
+function draw(){
+backgorund(tablero);
+}
